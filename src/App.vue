39d354ed123541ec39f844509ab1678c33a8d77c
Ali Sarac <alisarac@gmail.com>
2018-12-05 00:01:21 +0300

    Priest skills are added

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <button @click="addBinds"> Add New Bind</button>
     <ul>
-      <li v-for="bind in binds">
-        <bind :key="bind"/>
+      <li v-for="spell in spells"  :key="spell.bindKey">
+        <bind :spell="spell"/>
       </li>
     </ul>
 
@@ -21,7 +19,42 @@ export default {
   },
   data () {
     return {
-      binds: [],
+      spells: [
+        {bind: '1', name: 'Power Word: Shield', icon: 'spell_holy_powerwordshield'},
+        {bind: '2', name: 'Penance', icon: 'spell_holy_penance'},
+        {bind: '3', name: 'Shadow Mend', icon: 'spell_shadow_shadowmend'},
+        {bind: '4', name: 'Power Word: Radiance', icon: 'spell_priest_power-word'},
+        {bind: 'ctrl+1', name: 'Rapture', icon: 'spell_holy_rapture'},
+        {bind: 'ctrl+2', name: 'Pain Supression', icon: 'spell_holy_painsupression'},
+        {bind: 'ctrl+3', name: 'Power Word: Barrier', icon: 'spell_holy_powerwordbarrier'},
+        {bind: 'ctrl+4', name: 'Desperate Prayer', icon: 'spell_holy_testoffaith'},
+        {bind: 'r', name: 'Premonition', icon: 'inv_helm_robe_raidpriest_k_01'},
+        {bind: 'f', name: 'Mind Control', icon: 'spell_shadow_shadowworddominate'},
+        {bind: '5', name: 'Purge Of the Wicked', icon: 'ability_mage_firestarter'},
+        {bind: '6', name: 'Smite', icon: 'spell_holy_holysmite'},
+        {bind: '7', name: 'Mindbender', icon: 'spell_shadow_soulleech_3'},
+        {bind: '8', name: 'Dread Gladiator\'s Badge', icon: 'spell_holy_championsbond'},
+        {bind: '9', name: 'Power Word: Solace', icon: 'ability_priest_flashoflight'},
+        {bind: '0', name: 'Angelic Feather', icon: 'ability_priest_angelicfeather'},
+        {bind: 'f5', name: 'Mass Dispel', icon: 'spell_arcane_massdispel'},
+        {bind: 'f6', name: 'Psychic Scream', icon: 'spell_shadow_psychicscream'},
+        {bind: 'f7', name: 'Dark Archangel', icon: 'ability_priest_darkarchangel'},
+        {bind: 'f8', name: 'Gladiator\'s Medallion', icon: 'ability_pvp_gladiatormedallion'},
+        {bind: 'f9', name: 'Dispel Magic', icon: 'spell_nature_nullifydisease'},
+        {bind: 'f10', name: 'Will of the Forsaken', icon: 'spell_shadow_raisedead'},
+        {bind: 'ctrl+5', name: 'Archangel', icon: 'ability_priest_archangel'},
+        {bind: 'ctrl+6', name: 'Leap of Faith', icon: 'priest_spell_leapoffaith_a'},
+        {bind: 'ctrl+7', name: 'Power Word: Fortitude', icon: 'spell_holy_wordfortitude'},
+        {bind: 'ctrl+8', name: 'Yemek', icon: 'inv_misc_food_73cinnamonroll'},
+        {bind: 'ctrl+9', name: 'Holy Nova', icon: 'spell_holy_holynova'},
+        {bind: 'ctrl+0', name: 'Purify', icon: 'spell_holy_dispelmagic'},
+        {bind: 'ctrl+f5', name: 'Levitate', icon: 'spell_holy_layonhands'},
+        {bind: 'ctrl+f6', name: 'Resurrection', icon: 'spell_holy_resurrection'},
+        {bind: 'ctrl+f7', name: 'Mass Resurrection', icon: 'achievement_guildperk_massresurrection'},
+        {bind: 'ctrl+f8', name: 'Mount', icon: 'ability_mount_kodo_01'},
+        {bind: 'ctrl+f9', name: 'Fade', icon: 'spell_magic_lesserinvisibilty'},
+        {bind: 'ctrl+f10', name: 'Flying Mount', icon: 'ability_mount_netherdrakepurple'},
+      ],
       startKey: 1
     }
   },
--- src/components/Bind.vue
+++ src/components/Bind.vue
@@ -1,9 +1,7 @@
 <template>
   <div>
-    <input type="text" v-model="bindname"/>
-    <input type="text" v-model="iconname"/>
-    <img v-if="iconname" :src="icon"/>
-    {{ bindname }} - {{ bindkey }}
+    <img v-if="spell.icon" :src="icon"/>
+    {{ spell.name }} - {{ spell.bind }}
     <button @click="recordSequence()">Record Bind</button>
   </div>
 </template>
@@ -15,34 +13,26 @@ var Mousetrap = require('mousetrap-record')(require('mousetrap'));
 
 export default {
   name: 'bind-key',
-  props: {
-  },
+  props: ['spell'],
   data () {
     return {
-      bindkey: null,
-      bindname: null,
-      iconname: null,
-      editable: true,
-      previousKey: null
     }
   },
   computed: {
     icon() {
-      return "https://wow.zamimg.com/images/wow/icons/large/"+this.iconname+".jpg"
+      return "https://wow.zamimg.com/images/wow/icons/large/"+this.spell.icon+".jpg"
     }
   },
+  mounted(){
+    Mousetrap.bind([this.spell.bind], (e) => {
+      console.log(this.spell.bind, ' pressed ', this.spell.name)
+      return false;
+    });
+  },
+  unmounted(){
+    Mousetrap.unbind([this.spell.bind]);
+  },
   methods: {
-    setBind () {
-      console.log('setting bind', this.bindkey, this.bindname)
-      if(this.previousKey) {
-        Mousetrap.unbind([this.previousKey])
-      }
-      Mousetrap.bind([this.bindkey], () => {
-        this.previousKey = this.bindkey
-        console.log(this.bindkey, ' pressed ', this.bindname)
-        return false;
-      });
-    },
     recordSequence() {
       Mousetrap.record((sequence) => {
         this.bindkey = sequence.join(' ')
